31fc01f59bf21db2f8351007cead920bc03d365f
Tony Meyer <tony.meyer@canonical.com>
2024-04-11 18:49:00 +1200

    Tell type checks that the config options are strings.

--- src/charm.py
+++ src/charm.py
@@ -6,6 +6,7 @@
 
 import logging
 import socket
+import typing
 from urllib.parse import urlparse
 
 import yaml
@@ -237,7 +238,7 @@ class BlackboxExporterCharm(CharmBase):
         jobs = []
         external_url = urlparse(self._external_url)
         f"{external_url.path.rstrip('/')}/probe"
-        probes_scrape_jobs = self.model.config.get("probes_file")
+        probes_scrape_jobs = typing.cast(typing.Optional[str], self.model.config.get("probes_file"))
         if probes_scrape_jobs:
             probes = yaml.safe_load(probes_scrape_jobs)
             # Add the Blackbox Exporter's `relabel_configs` to each job

70c1b9b85ce4ac6eb7bb1821476f2d9f990422a9
Luca Bello <36242061+lucabello@users.noreply.github.com>
2025-04-07 09:26:45 +0200

    feat: migrate to ci v1 (#63)
    * feat: migrate to ci v1
    
    * fix alert linting
    
    * use CHARM_PATH in itests
    
    * fix project name
    
    * use charmcraft yaml in tests instead of metadata
    
    * charmcraft fetch-lib

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -16,6 +16,7 @@ logger = logging.getLogger(__name__)
 
 async def get_unit_address(ops_test: OpsTest, app_name: str, unit_num: int) -> str:
     """Get private address of a unit."""
+    assert ops_test.model
     status = await ops_test.model.get_status()  # noqa: F821
     return status["applications"][app_name]["units"][f"{app_name}/{unit_num}"]["address"]
 
@@ -30,10 +31,13 @@ async def is_blackbox_unit_up(ops_test: OpsTest, app_name: str, unit_num: int):
 
 
 async def is_blackbox_up(ops_test: OpsTest, app_name: str):
+    assert ops_test.model
+    application = ops_test.model.applications[app_name]
+    assert application
     return all(
         [
             await is_blackbox_unit_up(ops_test, app_name, unit_num)
-            for unit_num in range(len(ops_test.model.applications[app_name].units))
+            for unit_num in range(len(application.units))
         ]
     )
 

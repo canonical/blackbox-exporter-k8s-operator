6c450a8900c39eb54247b252b2125be16440035b
Michael Thamm <mike.thamm@canonical.com>
2026-06-15 12:22:12 -0400

    fix(track/2): Allow HTTPS in external URL (#105)

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -96,3 +96,11 @@ class TestWithoutInitialHooks(unittest.TestCase):
         self.assertIsInstance(self.harness.charm.unit.status, ops.model.ActiveStatus)
 
         self.assertEqual(self.harness.model.unit.name, "blackbox-exporter-k8s/0")
+
+    @k8s_resource_multipatch
+    @patch("charm.BlackboxExporterCharm._external_url", new="https://0.0.0.0/")
+    @patch.object(WorkloadManager, "_blackbox_exporter_version", property(lambda *_: "0.0.0"))
+    @patch.object(WorkloadManager, "push_config")
+    def test_unit_status_around_pebble_ready_with_https_external_url(self, *unused):
+        self.harness.container_pebble_ready(self.container_name)
+        self.assertIsInstance(self.harness.charm.unit.status, ops.model.ActiveStatus)
